29c91093c7ea759b64a4a4bd8853e418c7027685
Firmansyah <fsyah7052@gmail.com>
2020-10-13 20:42:02 +0700

    clean comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
 import '_layouts/App.dart';
 
-// void main() => runApp(MaterialApp (
-//   debugShowCheckedModeBanner: false,
-//   theme: ThemeData(primaryColor: Colors.red),
-//   home: App(),
-// ));
-
 void main() => runApp(App());
