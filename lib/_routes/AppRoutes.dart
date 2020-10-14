6708e0900150a643aaf10c6bcaeaed51388327d3
Firmansyah <fsyah7052@gmail.com>
2020-10-14 17:25:19 +0700

    Tampilan Admin

--- lib/_routes/AppRoutes.dart
+++ lib/_routes/AppRoutes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/widgets.dart';
 
 import 'package:flutter_app/views/View_Main.dart';
+import 'package:flutter_app/views/admin/View_Penjual.dart';
 
 // tempat routes
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  "/": (context) => View_Main(),
+  // "/": (context) => View_Main(),
+  "/datapenjual": (context) => View_Penjual(),
 };
