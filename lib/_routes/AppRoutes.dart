dcaff735b89ceb787a24c23b60abd0ee8b30dfa5
Rahmad Firmansyah <fsyah7052@gmail.com>
2021-01-15 19:21:35 +0700

    deleteall

--- lib/_routes/AppRoutes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-import 'package:flutter_app/views/View_Main.dart';
-
-final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  "/": (context) => View_Main(),
-  "/member": (context) => member.View_Index(),
-  "/member/show": (context) => member.View_Show(),
-};
